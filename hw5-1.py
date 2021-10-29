91e7fbb9707f51e7a4700d288e4244c00d539561
p22rlugo <p22rlugo@fairfieldprep.org>
2021-10-29 11:11:45 -0400

    Add file

--- hw5-1.py
+++ hw5-1.py
@@ -19,26 +19,20 @@ r.randint(0,20)
 
 # Question 6
 table = ['cat','dog','sheep','cow','chicken','pig']
-random_number = r.randint(0,6)
-animal_chosen = table[random_number]
+animal_chosen = r.choice(table)
 print(animal_chosen,"Was chosen")
 
 # Question 7
-print(r.randint(1,10))
-print(r.randint(1,10))
-print(r.randint(1,10))
-print(r.randint(1,10))
+random_10 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+print(r.choices(random_10, k=4))
 
 # Question 8
-print(r.randint(1,5))
-print(r.randint(1,5))
-print(r.randint(1,5))
-print(r.randint(1,5))
-print(r.randint(1,5))
+random_5 = [1,2,3,4,5]
+print(r.choices(random_5, k=5))
 
 # Question 9
 cards = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52]
-card_chosen = cards[r.randint(0,52)]
+card_chosen = r.choice(cards)
 print(card_chosen,"was the card # chosen.")
 
 # Question 10
